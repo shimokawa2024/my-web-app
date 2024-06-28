f8afd0e79b624e9dfa8f5c4aee58879433927b4b
shimokawa2024 <kd1367227@st.kobedenshi.ac.jp>
2024-06-28 10:42:02 +0900

    kadai09

--- kadai09_streamlit.py
+++ kadai09_streamlit.py
@@ -6,18 +6,18 @@ import plotly.express as px
 # タイトルと説明
 st.title('世界の国々の人口の推移')
 st.write("""
-このアプリケーションでは、世界の国々の人口の推移をインタラクティブに表示します。
+このアプリケーションでは、世界の国々の人口の推移をs表示します。
 """)
 # データをロード
 df = px.data.gapminder()
 # ユーザーインターフェース
-selected_country = st.selectbox('国を選択してください:', df['Country'].unique())
+selected_country = st.selectbox('国を選択してください:', df['country'].unique())
 
 # 選択された国のデータをフィルタリング
-filtered_df = df[df['Country'] == selected_country]
+filtered_df = df[df['country'] == selected_country]
 
 # Plotlyを使用してインタラクティブなグラフを作成
-fig = px.line(filtered_df, x='Year', y='Population', title=f'{selected_country}の人口の推移')
+fig = px.line(filtered_df, x='year', y='pop', title=f'{selected_country}の人口の推移')
 
 # グラフを表示
 st.plotly_chart(fig)
